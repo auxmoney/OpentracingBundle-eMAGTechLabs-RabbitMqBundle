e76b7844b8710aad16a250c333cb8023c555fbc1
Christian Alexander Wolf <cawolf@users.noreply.github.com>
2020-12-28 10:37:21 +0100

    fix: replace Travis with GitHub (#3)

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+shopt -s extglob
 
 cd build/testproject/
 composer config extra.symfony.allow-contrib true
-composer config repositories.origin vcs https://github.com/${PR_ORIGIN}
-composer require auxmoney/opentracing-bundle-emagtechlabs-rabbitmqbundle:dev-${BRANCH}
+composer require auxmoney/opentracing-bundle-emagtechlabs-rabbitmqbundle
+rm -fr vendor/auxmoney/opentracing-bundle-emagtechlabs-rabbitmqbundle/*
+cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-emagtechlabs-rabbitmqbundle
+composer dump-autoload
 cd ../../
